44f4ac0492bfb404dd0109d5b324b488b98b840b
Juanma Barea Martinez <juanmabareamartinez@gmail.com>
2024-04-29 12:31:53 +0200

    Adds notebooks

---
+++ notebooks/test-s3-connection.py
@@ -0,0 +1,24 @@
+import boto3, os
+
+BUCKET = os.getenv("AWS_S3_BUCKET")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+REGIONNAME = os.getenv("AWS_DEFAULT_REGION")
+AWS_S3_ENDPOINT = os.getenv("AWS_S3_ENDPOINT")
+
+ 
+FILENAME_ON_S3 = 'file.txt'
+FILE_ON_DISK = 'file.txt'
+ 
+file = open(f"{FILE_ON_DISK}", "w")
+file.write("Hello S3 Bucket! \n")
+file.close()
+ 
+s3 = boto3.resource('s3',endpoint_url = AWS_S3_ENDPOINT + ":9000",
+                    aws_access_key_id=AWS_ACCESS_KEY_ID,
+                    aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+ 
+s3.Bucket(BUCKET).upload_file(FILE_ON_DISK, FILENAME_ON_S3)
+ 
+ 
+print('uploading complete')
